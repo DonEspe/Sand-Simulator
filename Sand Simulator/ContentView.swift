c72878960e0fd0c3191eb6b5ae697315ccb590a4
Don Espe <Don.Espe@gmail.com>
2024-01-17 17:09:37 -0600

    Initial Commit

---
+++ Sand Simulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sand Simulator
+//
+//  Created by Don Espe on 1/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sand Simulator/Sand_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sand_SimulatorApp.swift
+//  Sand Simulator
+//
+//  Created by Don Espe on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Sand_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
